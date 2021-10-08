92635dbfa14b3f64240c05af21fbb3ca21801bb6
LeninneBP <leninne.bpirovano@gmail.com>
2021-10-08 16:15:00 -0400

    Build

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
-import Cadastro from './components/shared/painel/cadastro/Cadastro.vue';
-import Home     from './components/home/home.vue';
+const Cadastro = () => System.import('./components/shared/painel/cadastro/Cadastro.vue').then(m => m.default);
+import Home     from './components/home/home';
 
 export const routes = [
     { path: '', name : 'home', component: Home, titulo: 'Home', menu: true },
