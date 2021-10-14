dbf1aad92138b47b25d28bfc317c9ceaa29d8037
LeninneBP <leninne.bpirovano@gmail.com>
2021-10-14 15:14:56 -0400

    passando css para arquivo botao.scss

--- src/components/shared/botao/botao.vue
+++ src/components/shared/botao/botao.vue
@@ -50,27 +50,9 @@ export default{
     }
 }
 </script>
-<style lang="scss">
+<style lang="sass">
 
-    $cor: firebrick;    
-    
-    .botao {
-        display: inline-block;
-        padding: 10px;
-        border-radius: 3px;
-        margin: 10px;
-        font-size: 1.2em;
-    }
-
-    .botao-perigo {
-        background: $cor;
-        color: white;
-    }
-
-    .botao-padrao {
-        background: darkcyan;
-        color: white;
-    }
+    @import './botao.scss';
 
 </style>
 
